d4fe9cf7b7707fb50ff19f495b949dfafababb23
Udit Singh <2004udit@gmail.com>
2024-07-15 16:20:23 +0530

    composable_intro

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -4,10 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,6 +25,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -77,3 +89,96 @@ fun CardOne(modifier: Modifier = Modifier) {
 private fun CardOnePreview() {
     CardOne()
 }
+
+@Composable
+fun RatingCard(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .background(White)
+
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                imageVector = Icons.Default.Info,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(32.dp)
+                    .padding(8.dp)
+            )
+            Text(
+                text = "Do You LIke This Recommendation",
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "🤬")
+            Text(text = "☹️")
+            Text(text = "😐")
+            Text(text = "😊")
+            Text(text = "😍")
+
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "VERY BAD")
+            Text(text = "VERY GOOD")
+
+        }
+
+
+    }
+
+}
+
+
+
+
+@Preview
+@Composable
+private fun RatingCardPreview() {
+    RatingCard()
+}
+
+@Composable
+fun infoCard(modifier: Modifier = Modifier) {
+    Card (
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Row {
+            Column(
+                modifier = Modifier.weight(1f).height(300.dp)
+            ) {
+                Text(text = "welcome to the world of one piece")
+                Text(text = "Story start with execution of Gol.D.Roger")
+                Text(text = "Main protagonist is Monkey.D.Luffy")
+
+            }
+            Image(painter = painterResource(id = R.drawable.one_piece), contentDescription = null,
+                modifier = Modifier.weight(01f).height(300.dp))
+        }
+    }
+
+}
+
+@Preview
+@Composable
+private fun infoCardPreview() {
+    infoCard()
+
+}
+
+
+
