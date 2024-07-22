466e2942019faab53874d170b1f2695eb6fe5287
Udit Singh <2004udit@gmail.com>
2024-07-22 14:26:33 +0530

    composable_intro3_making_cards_scrollable

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -14,9 +14,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -24,6 +28,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -39,8 +45,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TestAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                        JJKList(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -180,5 +185,79 @@ private fun infoCardPreview() {
 
 }
 
+@Composable
+fun JJKList(modifier: Modifier = Modifier) {
+    val jjk = listOf(
+        "Monkey.D.luffy", "Roronoa zoro", "Nami",
+        "Brook", "Jimbe",
+    )
+    val jjkImages = listOf(
+        R.drawable.luffy, R.drawable.zoro,
+        R.drawable.nami ,R.drawable.brook,
+        R.drawable.jimbei,
+    )
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.fillMaxSize().padding(16.dp)
+    ) {
+        itemsIndexed(jjk) { index, name ->
+            val imgScale = 1f
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.background
+                ),
+                modifier = Modifier.padding(8.dp).width(300.dp),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 8.dp
+                ),
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(id = jjkImages[index]),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .weight(1f)
+                            .size(200.dp)
+                            .drawBehind {
+                                drawCircle(
+                                    color = Color.Blue.copy(alpha = 0.1f),
+                                    radius = 400f,
+                                )
+                                drawContext.transform.scale(
+                                    scaleX = imgScale,
+                                    scaleY = imgScale
+                                )
+                                drawContext.transform.translate(
+                                    left = 0f,
+                                    top = 100f
+                                )
+                                drawCircle(
+                                    color = Color.Red.copy(alpha = 0.2f),
+                                    radius = 300f,
+                                )
+
+                            }
+                    )
+                    Text(
+                        text = name,
+                        modifier = Modifier.weight(1f),
+                        style = MaterialTheme.typography.headlineLarge,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun JJKListPrevieew() {
+    JJKList()
+}
 
 
